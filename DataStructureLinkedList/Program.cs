09400a379e0b5a0d700aa361b5b67802ab296562
navitha <navithareddy112@gmail.com>
2022-09-14 09:56:59 +0530

    [navitha] UC1.SimpleLinkedList

---
+++ DataStructureLinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose option 1.add ele to list");
+            int option = Convert.ToInt32(Console.ReadLine());   
+            switch(option)
+            {
+                case 1:
+
+                    SimpleLinkedList list = new SimpleLinkedList();
+
+                    list.AddEle(34);
+                    list.AddEle(67);
+                    list.AddEle(89);
+                    list.AddEle(58);
+
+                    list.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter wrong choice");
+                    break;            
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructureLinkedList/SimpleLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    public class SimpleLinkedList
+    {
+        Node head;
+        public void AddEle(int newdata)
+        {
+            Node newNode = new Node(newdata);
+            if(this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;   
+                }
+                temp.next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+                Console.WriteLine("List is null");
+
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+            }
+
+        }
+
+    }
+}
